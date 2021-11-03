37cd7b0127e7006771fcce5503cebeb3ce2ba01c
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-11-03 11:29:40 -0500

    Made the output of the calculator methods more user friendly

--- calculator.js
+++ calculator.js
@@ -1,38 +1,47 @@
 const calc = {
+    addSign: '+',
+    subtractSign: '-',
+    multiplySign: '*',
+    divideSign: '/',
+    remainderSign: '%',
+    exponentSign: '**',
+    rootSign: '**',
+
     add(a,b) {
         const num1 = Number(a);
         const num2 = Number(b);
-        console.log(num1 + num2);
+        console.log(`${num1} + ${num2} = ${num1 + num2}`);
     },
     subtract(a,b) {
         const num1 = Number(a);
         const num2 = Number(b);
-        console.log(num1 - num2);
+        console.log(`${num1} - ${num2} = ${num1 - num2}`);
     },
     multiply(a,b) {
         const num1 = Number(a);
         const num2 = Number(b);
-        console.log(num1 * num2);
+        console.log(`${num1} * ${num2} = ${num1 * num2}`);
     },
     divide(a,b) {
         const num1 = Number(a);
         const num2 = Number(b);
-        console.log(num1 / num2);
+        console.log(`${num1} / ${num2} = ${num1 / num2}`);
     },
     remainder(a,b) {
+
         const num1 = Number(a);
         const num2 = Number(b);
-        console.log(num1 % num2);
+        console.log(`${num1} % ${num2} = ${num1 % num2}`);
     },
     exponent(a,b) {
         const num1 = Number(a);
         const num2 = Number(b);
-        console.log(num1 ** num2);
+        console.log(`${num1} ** ${num2} = ${num1 ** num2}`);
     },
     root(a,b) {
         const num1 = Number(a);
         const num2 = Number(b);
-        console.log(num1 ** (1 / num2));
+        console.log(`${num1} root of ${num2} = ${num2  ** (1 / num1)}`);
     }
 }
 
